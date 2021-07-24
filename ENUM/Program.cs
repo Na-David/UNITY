f0e8e87b65f3cee199fd841830850ea1f8d757bd
Aaron NA <hna206@naver.com>
2021-07-25 01:28:11 +1200

    Add project files.

---
+++ ENUM/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENUM
+{
+
+    enum PLAYERJOB
+    {
+
+    }
+
+    enum ITEMTYPE
+    {
+        Equip, Portion, Quest
+    }
+
+    class Item
+    {
+        public ITEMTYPE itemType = ITEMTYPE.Portion;
+    }
+
+    // Class, Struct, Enum
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Item NewItem = new Item();
+            NewItem.itemType = ITEMTYPE.Quest;
+        }
+    }
+}

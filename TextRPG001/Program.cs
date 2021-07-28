1807ca385c062a01bef04789f002f8aa9d64f90d
Aaron NA <hna206@naver.com>
2021-07-28 12:16:01 +1200

    ConsoleKeyInfo Practice

---
+++ TextRPG001/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextRPG001
+{
+    class Program
+    {
+        static int TenReturn() 
+        { 
+            return 10; 
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ConsoleKeyInfo KeyInfo = Console.ReadKey();
+                Console.WriteLine(KeyInfo.Key);
+            }
+        }
+    }
+
+    class Player
+    {
+
+    }
+
+    class Monster
+    {
+
+    }
+}

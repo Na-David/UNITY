3f83de6be706e5e741c79a369178fd77e429e522
David Na <1814729@nzse.ac.nz>
2021-10-12 01:12:38 +1300

    overriding

---
+++ Overriding/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overriding
+{
+
+    class FightUnit
+    {
+        protected string name = "NONE";
+        protected int ATK;
+        protected int HP;
+        public void Damage(FightUnit _OtherFightUnit)
+        {
+            Console.WriteLine( _OtherFightUnit.ATK + "damaged" + "From" + _OtherFightUnit.name );
+            HP -= _OtherFightUnit.ATK;
+        }
+    }
+
+    class Player : FightUnit
+    {
+        int ItemATK = 5;
+
+        public Player(string _Name)
+        {
+            name = _Name;
+        }
+    }
+
+    class Monster : FightUnit
+    {
+
+        public Monster(string _Name)
+        {
+            name = _Name;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player newPlayer = new Player("User");
+            Monster newMonster = new Monster("Fly");
+
+            newPlayer.Damage(newMonster);
+        }
+    }
+}
---
+++ Overriding2/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Overriding2
+{
+    class FightUnit
+    {
+        protected string name = "NONE";
+        protected int ATK = 10;
+        protected int HP;
+
+        public virtual int GetATK()
+        {
+            Console.WriteLine("FightUnit's ATK");
+            return ATK;
+        }
+        public void Damage(FightUnit _OtherFightUnit)
+        {
+            int ATK = _OtherFightUnit.GetATK();
+            Console.WriteLine(ATK + " damage " + "From " + _OtherFightUnit.name);
+            HP -= ATK;
+        }
+    }
+
+    class Player : FightUnit
+    {
+        int ItemATK = 5;
+
+        public override int GetATK()
+        {
+            return ATK + ItemATK;
+        }
+        public Player(string _Name)
+        {
+            name = _Name;
+        }
+    }
+
+    class Monster : FightUnit
+    {
+        int MonsterLv = 10;
+
+        public override int GetATK()
+        {
+            return ATK + MonsterLv;
+        }
+        public Monster(string _Name)
+        {
+            name = _Name;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player newPlayer = new Player("User");
+            Monster newMonster = new Monster("Fly");
+
+
+            newPlayer.Damage(newMonster);
+            newMonster.Damage(newPlayer);
+        }
+    }
+}
